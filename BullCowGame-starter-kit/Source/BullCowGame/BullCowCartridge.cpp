41f4948de47320da2b703071bb49b8bff020d9d1
Yash Kulthe <yashkulthex@gmail.com>
2020-01-17 23:01:16 +0530

    Pre and Post Increment!
    Do pre and post increment and work on the subtracting of the lives from the game.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,22 +4,89 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome To Bulls and Cows"));
-    PrintLine(TEXT("Guess the 3 letter word"));
-    PrintLine(TEXT("Press Enter To Continue"));
-    HiddenWord=TEXT ("jos");
+    
+    SetUpGame();
+
+    PrintLine(TEXT("The Hidden Word is:- %s. \nIt is %i characters long!"),*HiddenWord, HiddenWord.Len()); 
+  
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-    if(HiddenWord==Input)
+
+    if (bGameOver)
     {
-        PrintLine(TEXT("Your guess is right"));
+        ClearScreen();
+        SetUpGame();
     }
-    else
+    else  //checking PlayerGuess
     {
-        PrintLine(TEXT("Try again"));
+        
+        if(HiddenWord==Input)
+        {
+            PrintLine(TEXT("You have won!"));
+            EndGame();
+        }
+        else
+        {       
+            if(Input.Len() != HiddenWord.Len())
+            {
+                PrintLine(TEXT("The hidden word is %i characters long. \nYou have Lost!"),HiddenWord.Len());
+            EndGame();
+            }
+            else
+            {
+                --Lives;
+                PrintLine(TEXT("Lost a Life!"));
+                if(Lives>0)
+                {
+                
+                    PrintLine(TEXT("Your Guess is incorrect! \nYour remaining Lives are: %i"),Lives);
+                }
+                else
+                {
+                    PrintLine(TEXT("You have no lives left!"));
+                    EndGame();
+                }
+                
+            }
+            
+        }
     }
-    
+
+    //Check if isogram
+    //Prompt to guess again
+
+    //Check right no of characters
+    //Prompt to guess again
+
+    //Remove Life
+    //Check if  lives>0
+    //If yes GuessAgain
+    //Show Lives Left
+    //if no show GameOver and the HiddenWord
+    //Prompt to play again, Press Enter to Play Again?
+    //Check user input
+    //PlayAgain or quit
+}
+
+void UBullCowCartridge::SetUpGame()
+{
+    //Welcoming the player
+    PrintLine(TEXT("Welcome To Bulls and Cows"));
+
+    HiddenWord=TEXT ("jos");  
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+  
+    PrintLine(TEXT("Guess the %i letter word"),HiddenWord.Len());
+    PrintLine(TEXT("Your Total Lives are: %i"),Lives);
+    PrintLine(TEXT("Type in your guess.\nPress Enter To Continue..."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver=true;
+    PrintLine(TEXT("Press Enter TO Play Again."));
 }
